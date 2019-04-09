95a28e7656b78bc71f61ae73837488c57fa40df0
Eric Gaston Haas <ericgaston1234@gmail.com>
2019-04-09 03:32:02 -0300

    Cliente
    Agrego clase cliente con Setters y Getters

--- src/Inicio.java
+++ src/Inicio.java
@@ -1,10 +1,5 @@
 
 public @interface Inicio {
 
-/*
- * esto es para hacer algo despues se borra, la carpeta Historial es para guardar 	
- * texto y el xml de los diagramas, ahora me acuesto y mañana seguimos
- */
-	
 	
 }
---
+++ src/entity/Cliente.java
@@ -0,0 +1,49 @@
+package entity;
+
+public class Cliente {
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private String dni;
+	private String numero_cuenta;
+	private int deuda;
+	
+	public void SetNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public void SetApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public void SetDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public void SetDni(String dni) {
+		this.dni = dni;
+	}
+	public void SetNumeroCuenta(String numero_cuenta) {
+		this.numero_cuenta = numero_cuenta;
+	}
+	public void SetDeuda(int deuda) {
+		this.deuda = deuda;
+		
+	}
+	public String GetNombre() {
+		return nombre;
+	}
+	public String GetApellido() {
+		return apellido;
+	}
+	public String GetDireccion() {
+		return direccion;
+	}
+	public String GetDni() {
+		return dni;
+	}
+	public String GetNumeroCuenta() {
+		return numero_cuenta;
+	}
+	public int GetDeuda() {
+		return deuda;
+	}
+
+}	
