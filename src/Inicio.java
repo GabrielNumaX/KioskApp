0a476c09c6ffb65f71abe86bd0ecbe693ef78a5f
GabrielNumaX <>
2019-04-09 00:39:24 -0300

    Inicio proyecto 09.04
    creacion de estructura de carpetas

---
+++ src/Inicio.java
@@ -0,0 +1,10 @@
+
+public @interface Inicio {
+
+/*
+ * esto es para hacer algo despues se borra, la carpeta Historial es para guardar 	
+ * texto y el xml de los diagramas, ahora me acuesto y mañana seguimos
+ */
+	
+	
+}
