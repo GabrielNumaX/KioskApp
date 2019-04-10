efd092c8f75a427d856721ea48293d0ee0d0fe03
Eric Gaston Haas <ericgaston1234@gmail.com>
2019-04-10 02:32:16 -0300

    Nuevos cambios

---
+++ src/kioskapp/controller/Principal.java
@@ -0,0 +1,38 @@
+package kioskapp.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class Principal extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("kioskapp/view/Principal.fxml"));
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			
+			Screen screen = Screen.getPrimary();
+			Rectangle2D bounds = screen.getVisualBounds();            
+
+			stage.setX(bounds.getMinX());
+			stage.setY(bounds.getMinY());
+			stage.setWidth(bounds.getWidth());
+			stage.setHeight(bounds.getHeight());
+		
+			stage.show();
+		}catch(Exception e) {
+			System.out.println("Error: "+e);
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
